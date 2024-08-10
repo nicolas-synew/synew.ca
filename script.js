a54cab3110181eb1c7bd1aff1a7dd0030c8bfe55
MrWaffleMaker <nnjoo@hotmail.com>
2024-08-09 22:50:14 -0400

    Initial commit with website files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 1200, // Animation duration in milliseconds
+        once: true, // Animation should happen only once
+    });
+});
